3818edae756a2eebfe18992227a20ca7375f5201
LuisAcevedo777 <japay01@hotmail.com>
2023-01-30 15:25:55 -0500

    thirtEntregable

--- src/components/AppNavbar.jsx
+++ src/components/AppNavbar.jsx
@@ -11,16 +11,15 @@ const AppNavbar = () => {
             <Navbar className='pnav  fixed-top bg-info' expand="xl">
            
                 <div className='nav d-flex justify-content-between fixed-top '>
-                    <div className='nav1 text-danger'>E-commerce</div>
+                <Navbar.Brand as={Link} to='/' className='nav1 text-danger' >E-commerce</Navbar.Brand>
                   <div className='nav2'>
                     
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                        <Navbar.Brand as={Link} to='/'>Home</Navbar.Brand>
-                            <Nav.Link as={Link} to="/Login" className='text-black'>Login</Nav.Link>
-                            <Nav.Link as={Link} to="/Purchases" className='text-black'>Purchases</Nav.Link>
-                            <Nav.Link className='text-black'>DetailProduct</Nav.Link>
+                            <Nav.Link as={Link} to="/Login" className='l1 text-black'><i class="fa-solid fa-user fa-3x"></i></Nav.Link>
+                            <Nav.Link as={Link} to="/Purchases" className='l1 text-black'><i class="fa-solid fa-box-archive fa-3x"></i></Nav.Link>
+                            <Nav.Link className='l1 text-black'><i class="fa-solid fa-cart-shopping fa-3x"></i></Nav.Link>
 
 
                         </Nav>
