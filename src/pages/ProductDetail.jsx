5384893e8b8331941b711c1e8ef70849d0c575c9
LuisAcevedo777 <japay01@hotmail.com>
2023-01-30 15:37:40 -0500

    forthEntregable

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -3,8 +3,9 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams, useNavigate } from 'react-router-dom'
 import { filterProductsCategoryThunk } from '../store/slices/productsAll.slice';
-import { Row, Col } from 'react-bootstrap'
+import { Row, Col, Carousel } from 'react-bootstrap'
 import '../App.css'
+import {Link} from 'react-router-dom'
 
 
 const ProductDetail = () => {
@@ -43,18 +44,56 @@ const ProductDetail = () => {
     return (
         <div>
             <Row>
-                <Col lg={9}>
+                <Col lg={7}>
                     <div className='col1'>
-                        <h1>{uniqueProduct.title}</h1>
-                        <img className='imgUnique' src={uniqueProduct.images?.[0].url} alt="" />
+                       <div className='linktotit'><Link className='linkHome' to={'/'}>Home</Link> </div>
+                       
+                       <Carousel>
+                       
+      <Carousel.Item>
+    
+        <img
+          className='imgUnique'
+          src={uniqueProduct.images?.[0].url}
+          alt="First slide"
+        />
+     
+      </Carousel.Item>
+      <Carousel.Item>
+     
+        <img
+          className='imgUnique'
+          src={uniqueProduct.images?.[1].url}
+          alt="Second slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+      
+        <img
+          className='imgUnique'
+          src={uniqueProduct.images?.[2].url}
+          alt="Third slide"
+        />
+
+      </Carousel.Item>
+    </Carousel>
+                       
 
                         <p>Price: {uniqueProduct.price}</p></div>
                 </Col>
-                <Col lg={3}>
+                <Col lg={5}>
+                    <div className='descriptionTot'>
                     <h2>DESCRIPTION:</h2>
                     <p>{uniqueProduct.description}</p>
+                   <div className='pricValue'><h2 className='pricpric'><span className='priceD'>Price:</span>{uniqueProduct.price} </h2>
+                   <div className='buttonDesc'>
+                   <button className='count'>-</button>
+                   <div className='count'></div>
+                   <button className='count'>+</button></div>
+                   </div> 
+                   <button className='addToCart'>Add to Cart</button>
+                   </div>
                 </Col></Row>
-
 
 
             <ul className='ulistSuggered'>
@@ -65,6 +104,7 @@ const ProductDetail = () => {
 
                             <img className='imgSuggered' src={productSuggested.images[0].url} alt="" />
                             <p className='titleSuggered'>{productSuggested.title}</p>
+                            <button className='cartButton1'><i class="fa-solid fa-cart-shopping"></i></button>
                         </li>
 
                     ))
